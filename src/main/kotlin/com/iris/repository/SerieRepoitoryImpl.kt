2d83cdda56c99d430375c92f8c79464adace9f6b
Iris <irisbritox@gmail.com>
2021-06-22 09:24:31 -0300

    Adicionar implementação do método getById em SerieRepositoryImpl

--- src/main/kotlin/com/iris/repository/SerieRepoitoryImpl.kt
+++ src/main/kotlin/com/iris/repository/SerieRepoitoryImpl.kt
@@ -3,6 +3,8 @@ package com.iris.repository
 import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.api.core.cql.SimpleStatement
 import com.iris.entity.Serie
+import com.iris.exception.SerieNotFoundException
+import java.util.*
 
 class SerieRepoitoryImpl (private val cqlSession: CqlSession) : SerieRepository {
 
@@ -22,5 +24,31 @@ class SerieRepoitoryImpl (private val cqlSession: CqlSession) : SerieRepository 
                 serie.getString("where_to_watch")!!
             )
         }.toList()
+    }
+
+    override fun getById(id: UUID): Serie? {
+        try {
+            val queryResult = cqlSession.execute(
+                SimpleStatement
+                    .newInstance(
+                        "SELECT * FROM tv_shows.Series WHERE id= ?",
+                        id
+                    )
+            )
+
+            return queryResult.map { serie ->
+                Serie(
+                    serie.getUuid("id")!!,
+                    serie.getString("name")!!,
+                    serie.getString("description")!!,
+                    serie.getString("genre")!!,
+                    serie.getString("where_to_watch")!!
+                )
+            }.single()
+
+        } catch (e: RuntimeException) {
+            throw SerieNotFoundException()
+        }
+
     }
 }
