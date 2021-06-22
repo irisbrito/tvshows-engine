aabb7dc5364a0c0f0d122325153b7369717b4ea6
Iris <irisbritox@gmail.com>
2021-06-22 16:50:52 -0300

    Adicionar teste do método get all serie em SerieControllerTest

---
+++ src/test/kotlin/com/iris/controller/SerieControllerTest.kt
@@ -0,0 +1,31 @@
+package com.iris.controller
+
+import com.iris.entity.Serie
+import com.iris.service.SerieServiceImpl
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.mockk.every
+import io.mockk.mockk
+import java.util.*
+
+@MicronautTest
+class SerieControllerTest : AnnotationSpec() {
+
+    val service = mockk<SerieServiceImpl>(relaxed = true)
+    val serieController = SerieController(service)
+    lateinit var serie: Serie
+    val id : UUID = UUID.fromString("3a5fd8cc-96a5-4603-8de9-3a333fa28338")
+
+    @BeforeEach
+    fun setUp(){
+        serie = Serie(id, "The 100", "Série pós apocaliptica","Ficção-Cientifica", "Netflix")
+    }
+
+    @Test
+    fun `should get all series`(){
+        every {service.getAll()} answers {listOf(serie)}
+        val result = serieController.getSeries().body()
+        result shouldBe listOf(serie)
+    }
+}
