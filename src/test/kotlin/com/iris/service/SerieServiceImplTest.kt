ffbee45d6bf6ce024b43800ed4412f6b96c4b7b6
Iris <irisbritox@gmail.com>
2021-06-22 16:44:33 -0300

    Adicionar teste buscar série pelo id em SerieServiceImplTest

---
+++ src/test/kotlin/com/iris/service/SerieServiceImplTest.kt
@@ -0,0 +1,32 @@
+package com.iris.service
+
+import com.iris.entity.Serie
+import com.iris.repository.SerieRepositoryImpl
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.mockk.every
+import io.mockk.mockk
+import java.util.*
+
+@MicronautTest
+class SerieServiceImplTest : AnnotationSpec() {
+
+    val repository = mockk<SerieRepositoryImpl>(relaxed = true)
+    val serieService = SerieServiceImpl(repository)
+    lateinit var serie: Serie
+
+    val id : UUID = UUID.fromString("3a5fd8cc-96a5-4603-8de9-3a333fa28338")
+
+    @BeforeEach
+    fun setUp(){
+        serie = Serie(id, "The 100", "Série pós apocaliptica","Ficção-Cientifica", "Netflix")
+    }
+
+    @Test
+    fun `should get serie by id`(){
+        every {repository.getById(any())} answers {serie}
+        val result = serieService.getById(id)
+        result shouldBe serie
+    }
+}
