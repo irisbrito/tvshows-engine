0801c6f1baf587957df6c0d250258ffc58bbc366
Iris <irisbritox@gmail.com>
2021-06-25 17:03:21 -0300

    Adicionar conversor

---
+++ src/main/kotlin/com/iris/core/mapper/SerieConverter.kt
@@ -0,0 +1,39 @@
+package com.iris.core.mapper
+
+import com.iris.core.model.Serie
+import com.iris.database.entity.SerieEntity
+import com.iris.entrypoint.dto.SerieDto
+import java.util.stream.Collectors
+
+class SerieConverter {
+    companion object {
+        fun serieEntityToSerie(serieEntity: SerieEntity) =
+            Serie(
+                serieEntity.id,
+                serieEntity.name,
+                serieEntity.description,
+                serieEntity.genre,
+                serieEntity.whereToWatch
+            )
+
+        fun serieToSerieDto(serie: Serie) =
+            SerieDto(
+                serie.id,
+                serie.name,
+                serie.description,
+                serie.genre,
+                serie.whereToWatch
+            )
+
+        fun serieEntityListToSerieList(serieEntity: List<SerieEntity>) =
+            serieEntity.stream().map { serie ->
+                Serie(serie.id, serie.name,serie.description,serie.genre, serie.whereToWatch)
+            }.collect(Collectors.toList())
+
+        fun serieListToSerieDtoList(serieList : List<Serie>) =
+            serieList.stream().map { serie ->
+                SerieDto(serie.id, serie.name, serie.description, serie.genre, serie.whereToWatch)
+            }.collect(Collectors.toList())
+
+    }
+}
