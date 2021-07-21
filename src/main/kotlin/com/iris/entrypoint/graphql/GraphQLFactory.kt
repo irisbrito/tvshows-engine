b4e8827b90a53784da040fda4802bdc78f914b07
irissouzazup <irisbritox@gmail.com>
2021-07-21 09:39:31 -0300

    Adicionar GraphQLFactory

---
+++ src/main/kotlin/com/iris/entrypoint/graphql/GraphQLFactory.kt
@@ -0,0 +1,51 @@
+package com.iris.entrypoint.graphql
+
+import graphql.GraphQL
+import graphql.schema.idl.RuntimeWiring
+import graphql.schema.idl.SchemaGenerator
+import graphql.schema.idl.SchemaParser
+import graphql.schema.idl.TypeDefinitionRegistry
+import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Factory
+import io.micronaut.core.io.ResourceResolver
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import javax.inject.Singleton
+
+@Factory
+class GraphQLFactory {
+
+    @Bean
+    @Singleton
+    fun graphQL(resourceResolver: ResourceResolver,
+        series: SerieDataFetcher,
+        serieById: SerieByIdDataFetcher) : GraphQL {
+
+        val schemaParser = SchemaParser()
+        val schemaGenerator = SchemaGenerator()
+
+        // Parse the schema.
+        val typeRegistry = TypeDefinitionRegistry();
+        typeRegistry.merge(schemaParser.parse(BufferedReader(InputStreamReader(
+            resourceResolver.getResourceAsStream("classpath:schema.graphqls").get())
+            )
+        ))
+
+        // Create the runtime wiring.
+        val runtimeWiring = RuntimeWiring.newRuntimeWiring()
+            .type("Query")
+            { typeWiring -> typeWiring
+                .dataFetcher("findSerieById",serieById)}
+            .type("Query")
+            { typeWiring -> typeWiring
+                .dataFetcher("findAllSeries", series)
+            }
+            .build()
+
+        // Create the executable schema.
+        val graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring)
+
+        // Return the GraphQL bean.
+        return GraphQL.newGraphQL(graphQLSchema).build()
+    }
+}
